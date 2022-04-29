87561065009d29aefee5b3cd50532b3ebc8d6136
Fernando Buck <buck.s@hotmail.com>
2022-04-28 22:32:38 -0300

    Teste de validações em campos do form de cadastro de usuarios.

--- my-app/client/usuario/cadastraUsuario.js
+++ my-app/client/usuario/cadastraUsuario.js
@@ -8,13 +8,44 @@ $( document ).ready(function()
         const email = document.getElementById("id-email").value
         const login = document.getElementById("id-login").value
         const senha = document.getElementById("id-senha").value
-        const confirmarSenha = document.getElementById("id-confirmar-senha").value
+        const confirmaSenha = document.getElementById("id-confirmar-senha").value
 
-        validarCamposPreenchidos()
+        const objectDataValidaUsuarioForm = {
+            camposVazios : validarCamposPreenchidos(),
+            nomeValidado : validaNome(nome),
+            emailValidado : validaEmail(email),
+            loginValidado : validaLogin(login),
+            senhasIguais : validaSenhaIguais(senha, confirmaSenha),
+            senhaValidado : validaSenha(senha),
+            confirmaSenhaValidado : validaSenha(confirmaSenha)
+        }
+        
+
+        
+        if(objectDataValidaUsuarioForm.emailValido){
+            console.log("Email Valido")
+        }
+        if(objectDataValidaUsuarioForm.camposVazios){
+            console.log("Há campos vazios")
+        }
+        if(objectDataValidaUsuarioForm.nomeValidado){
+            console.log("Nome valido")
+        }
+        if(objectDataValidaUsuarioForm.loginValidado){
+            console.log("Login valido")
+        }
+        if(objectDataValidaUsuarioForm.senhasIguais){
+            console.log("Senhas iguais")
+        }
+        if(objectDataValidaUsuarioForm.senhaValidado){
+            console.log("Senha valida")
+        }
+        if(objectDataValidaUsuarioForm.confirmaSenhaValidado){
+            console.log("Senha de confirmação valida")
+        }
+        
 
     })
 
-    $("input").change(function(){
-        $('#div-erro-'+$(this).attr("name")).empty()
-    })
+    limpaCamposAoPreencher()
 })
--- my-app/client/validacoes.js
+++ my-app/client/validacoes.js
@@ -1,12 +1,60 @@
 function validarCamposPreenchidos(){
     
+    haCampoVazio = false
+
     $("input").each(function(){
         if($(this).val() == ""){
-            
+            haCampoVazio = true
             if( $('#div-erro-'+$(this).attr("name")) ){
-			    $('#div-erro-'+$(this).attr("name")).html("*Esse campo é obrigatório");
+			    $('#div-erro-'+$(this).attr("name")).html("*Esse campo é obrigatório.")
+                
 		    }
-		    
         }
     })
+    return haCampoVazio
+}
+
+function limpaCamposAoPreencher(){
+    $("input").change(function(){
+        $('#div-erro-'+$(this).attr("name")).empty()
+    })
+}
+
+function validaEmail(email){
+    const emailRe = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if(emailRe.test(email)){
+        return true
+    }
+    return false
+}
+
+function validaNome(nome){
+    const nomeRe = /^\s*([A-Za-z]{1,}([\.,] |[-']| ))*[A-Za-z]+\.?\s*$/
+    if(nomeRe.test(nome)){
+        return true
+    }
+    return false
+}
+
+function validaLogin(login){
+    const loginRe = /^([a-zA-Z0-9])+[\.]*([a-zA-Z0-9])+$/
+    if(loginRe.test(login)){
+        return true
+    }
+    else return false
+}
+
+function validaSenhaIguais(senha, confirmaSenha){
+    if(senha == confirmaSenha){
+        return true
+    }
+    return false
+}
+
+function validaSenha(senha){
+    const senhaRe = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/
+    if(senhaRe.test(senha)){
+        return true
+    }
+    return false
 }
