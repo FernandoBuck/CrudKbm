e6467be08c87169175ffb16c2c6fcd67e3bc02dd
Fernando Buck <buck.s@hotmail.com>
2022-05-25 20:13:38 -0300

    Desenvolvimento do envio da requisição front-end da tela de login.

---
+++ my-app/client/home/login.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() 
+{
+
+    loginCredential = localStorage.getItem("loginCredential")
+
+
+    $("#botao-login").click(async function(e)
+    {
+        e.preventDefault()
+
+        const login = document.getElementById("id-login").value
+        const senha = document.getElementById("id-senha").value
+
+        const objectDataLogin = {
+            login : login,
+            senha : senha
+        }
+
+        const response = await $.ajax
+        ({
+            type: 'POST',
+            url: '../../server/api/login.php',
+            data: {
+                funcao : "validaLogin",
+                dados : objectDataLogin
+            },
+        })
+    })
+})
